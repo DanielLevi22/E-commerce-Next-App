c0460e0bb044b3112f4d03261b81160f2cf93262
Daniel Levi <daniellevi000@gmail.com>
2024-05-23 11:42:29 -0300

    feat: craete test add-product-to cart && search product

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,48 @@
+describe('add product to cart', () => {
+
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+
+
+
+  it('should be able to navigation to the product page and add it to thee cart', () => {
+  
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.url().should('include', '/product')
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao Carrinho').click()
+    
+
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should not count duplicated product on cart', () => {
+  
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.url().should('include', '/product')
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao Carrinho').click()
+    cy.contains('Adicionar ao Carrinho').click()
+
+    
+    cy.contains('Cart (1)').should('exist')
+  })
+
+  it('should be able to search product for a product add it to the cart', () => {
+  
+    cy.get('input[name=q]').type('moletom').parent('form').submit()
+
+    cy.get('a[href^="/product"]').first().click()
+
+    cy.location('pathname').should('include', '/product')
+    cy.contains('Adicionar ao Carrinho').click()
+    cy.contains('Cart (1)').should('exist')
+
+  })
+})
